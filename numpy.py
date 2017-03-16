de8683c5d130e4cd9aa5408366ff1e9d6672faa5
owentemp <owentemple@gmail.com>
2017-03-15 19:40:51 -0500

    create numpy matrix with correct data type

--- numpy.py
+++ numpy.py
@@ -23,14 +23,39 @@ avg_year = total/len(years)
 
 
 
-
-
-
-
 #4 Using NumPy
 
 import numpy
 
 world_alcohol = numpy.genfromtxt("world_alcohol.csv", delimiter=",")
 print(type(world_alcohol))
+
+#5 Creating Arrays
+
+# Create a vector from the list [10, 20, 30].
+# Assign the result to the variable vector.
+# Create a matrix from the list of lists [[5, 10, 15], [20, 25, 30], [35, 40, 45]].
+# Assign the result to the variable matrix.
+
+
+vector = numpy.array([10, 20, 30])
+
+matrix = numpy.array([[5,10,15],[20,25,30],[35,40,45]])
+
+
+vector_shape = vector.shape
+
+matrix_shape = matrix.shape
+
+
+world_alcohol_dtype = world_alcohol.dtype
+
+
+
+world_alcohol = numpy.genfromtxt("world_alcohol.csv", dtype="U75", skip_header=1, delimiter = ",")
+
+print(world_alcohol)
+
+
+
 
