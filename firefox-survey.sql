8a637249240bbe0a949fbe3e58a4c10b064274e9
owentemp <owentemple@gmail.com>
2017-04-28 13:58:53 -0500

    queries to find distributions of bookmark and tab usage among current users

--- firefox-survey.sql
+++ firefox-survey.sql
@@ -1,5 +1,3 @@
--- You are chatting with an opinionated colleague of yours, and you ask her thoughts about your idea to revamp the bookmarks experience. She says that bookmarks aren't interesting and you should help her in her quest to improve support for having a large number of tabs. Your boss has asked you to put together a presentation recommending an area of focus for the next version of Firefox.
--- After considering the Data Analytics Workflow, you decide to look at a  recent test flight  with customer surveys and usage analytics to better inform your thinking. After that, you will decide which of the two areas to recommend.
 
 
 
@@ -26,18 +24,6 @@ SELECT q6, count(*) FROM survey GROUP BY q6 order by q6;
 -- Looks like this might be a young group and skewed male, so it might not be representative. Tere are 3,719 males and only 257 females in the sample. Also, most users (1,413) are in the 18-25 age group, so perhaps this is a college campus we are seeing?
 
 
-
-
-
--- Describe any initial findings related to your research questions.
-
-
--- Describe what you still need to do for your presentation in order to make a sound recommendation.
-
-
-
--- ● Summarize the data in your presentation:
--- ○ Present your findings, results, and recommendations to the class.
 -- ○ Review the Users table. Summarize the users represented in the survey.
 
 SELECT * FROM users LIMIT 100;
@@ -51,11 +37,10 @@ SELECT count(*) from users;
 
 -- ○ Review the Survey table. How many of the total users completed the survey?
 
---4,081 of them answered survey questions
+--4,081 of the 27,267 users answered survey questions (14.9%)
 
 
--- ○ Of users who completed the survey, identify the number of users who are new to
--- Firefox, as well as those who are longtime users.
+-- ○ Of users who completed the survey, identify the number of users who are new toFirefox, as well as those who are longtime users.
 
 SELECT q1, count(*), 100 * count(*)/(SELECT count(*) from survey) as pctoftotal from survey GROUP BY q1 order by q1;
 
@@ -159,7 +144,7 @@ WHERE event_code = 10
 GROUP BY user_id
 ORDER BY 2;
 
---broken apart by how long they used Firefox
+--broken apaverageart by how long they used Firefox
 
 SELECT survey.q1, count(*) FROM events
 JOIN survey using(user_id)
@@ -177,14 +162,20 @@ WHERE events.event_code = 8
 GROUP BY survey.q1
 ORDER BY survey.q1;
 
+SELECT survey.q1, ROUND (stddev_samp (CAST( rtrim(events.data1,' total bookmarks') as integer)),0) as bookmarks from events
+JOIN survey using(user_id)
+WHERE events.event_code = 8
+GROUP BY survey.q1
+ORDER BY survey.q1;
+
 --the longer people have used Firefox the more book marks they have and the more they use them
---494 for less than 3 months
---120 for 3 to 6 months
---219 for 6 months to a year
---794 for 1 to 2 years
---1639 chooses for 2 to 3 years
---4,129 chooses for 3 to 5 years
---4,240 chooses for more than 5 years
+--avg of 494 (stddev 126) chooses for less than 3 months group
+--avg of 120 (stddev of 25) chooses for 3 to 6 months group
+--avg of 219 (stddev of 275) chooses for 6 months to a year group
+--avg of 794 (stddev of 184) chooses for 1 to 2 years group
+--avg of 1639 (stddev of 450) chooses for 2 to 3 years group
+--avg of 4,129 (stddev of 812) chooses for 3 to 5 years group
+--avg of 4,240 (stddev of 771) chooses for more than 5 years group
 
 
 
@@ -212,6 +203,24 @@ WHERE event_code = 26;
 SELECT ROUND( stddev_samp (CAST( rtrim(data2,' tabs') as integer )),2) FROM events
 WHERE event_code = 26;
 --standard deviation is 32.96
+
+
+SELECT survey.q1, ROUND(AVG(CAST( rtrim(events.data2,' tabs') as integer )),0) FROM events
+JOIN survey using(user_id)
+WHERE events.event_code = 26
+GROUP BY survey.q1
+ORDER BY survey.q1;
+
+--the longer people have used Firefox the more tabs open they have
+--average of 8 (stddev of 11) for less than 3 months group
+--average of 2 (stddev of 2) for 3 to 6 months group
+--average of 4 (stddev of 8) for 6 months to a year group
+--average of 5 (stddev of 9) for 1 to 2 years group
+--average of 5 (stddev of 6) for 2 to 3 years group
+--average of 6 (stddev of 7) for 3 to 5 years group
+--average of 12 (stddev of 26) for more than 5 years group
+
+
 
 
 
@@ -256,46 +265,3 @@ ORDER BY 2;
 --8.4% have more than 15 tabs open
 
 
--- ● Compare the two features and create a coherent recommendation for your boss.
-
-
--- ○ Do you agree or disagree with your opinionated colleague?
--- ○ Do you think the team should go in a different direction?
--- ● Include analysis of data with statistics and outliers.
--- ● Include recommendation with predicted outcomes and identified action items.
--- ● Present any limitations and assumptions.
--- ● Identify follow-up problems and questions for future analysis.
-
--- 3. SQL Queries + Questions:
--- Due: beginning of Lesson 14 Format: .sql file
--- ● List the appropriate business questions you were trying to answer.
--- ● Use correct, efficient syntax for the functions and operators needed.
--- ● Include corresponding output listed in the .sql file.
-
-
-
--- GETTING STARTED
--- Consider the Data Analytics Workflow to guide you through each step:
--- ● Identify and Understand the Problem: After considering the prompt, think generally about the problem and how you might want to approach it. Ideally, what type of data would you have to make a decision and produce a recommendation?
--- ● Access and Obtain the Data: Your company, Mozilla, is running its own PostgreSQL server, and the engineers who designed the survey have added that data to three tables: events, survey, and users. Connect to the appropriate server using the software of your choice (pgAdmin III or DBVisualizer, for example).
--- ○ To connect to the server, please use the following login credential with the firefox_database:
--- ■ url/host:
--- ● West-coast (includes Austin and Chicago) -
--- analyticsga.cuwj8wuu6wbh.us-west-2.rds.amazonaws.com
--- ● East-coast - analyticsga-east2.c20gkj5cvu3l.us-east-1.rds.amazonaws.com
--- ● APAC- analyticsga-apac2.csuojbfcexhv.ap-southeast-2.rds.amazonaws.co m
--- ● Europe - analyticsga-west1.c8yztxlaoku1.eu-west-1.rds.amazonaws.com
--- ■ port: 5432
--- ■ username: analytics_student
--- ■ password: analyticsga
--- ● Understand the Data:   Your colleagues in the engineering department are very organized and have written documentation for the product managers who need to use their databases. Make sure you read it!  Link to documentation —note that the live site has been taken down but the web archive works great!
---       ○
--- HINT: Make sure to identify which user actions will be of most use to your research questions. Browse the tables and ensure you know how the data is organized and how the tables relate to each other. Answer basic questions about the data. When presenting the results of survey data, it’s often good to inform your audience about the characteristics of the surveyed users. Do you think these users are representative of all Firefox users? Why or why not? What other limitations might exist with this data?
--- ● Prepare, Structure, and Clean the Data: In order to be able to settle on your recommendation and create your presentation, you will need to write a number of queries to prepare, structure, filter, clean, and aggregate your data.
-
--- AN
--- Unit 2 Project
--- ● Analyze and Visualize the Data: In order to determine findings and create a recommendation, you will want to use statistical (mean, median, mode, min, max, etc.) functions as well as other aggregate functions (count) to summarize your data. Export your data to Excel to create any visualizations that will help you either explore your data or provide evidence for your recommendation.
--- ● Present the Results of Analysis: Based on your findings, create a coherent argument about which project is more worthwhile: tabs or bookmarks.
--- ● Create a PowerPoint presentation that summarizes your findings and makes a recommendation.
--- ● Make sure to include the results of your analysis and any visualizations that help boost your argument.
