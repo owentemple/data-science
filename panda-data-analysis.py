9ba43a10a53ec9a34809a9a1e92031a9e5d8e874
owentemp <owentemple@gmail.com>
2017-05-14 23:36:09 -0500

    calculate summary statistics -- need to review

--- panda-data-analysis.py
+++ panda-data-analysis.py
@@ -219,5 +219,14 @@ correct_mean_fare = titanic_survival["fare"].mean()
 #Through page 20
 
 
+# 6: Calculating Summary Statistics
 
+
+passenger_classes = [1, 2, 3]
+fares_by_class = {}
+for this_class in passenger_classes:
+    pclass_rows = titanic_survival[titanic_survival["pclass"] == this_class]
+    pclass_fares = pclass_rows["fare"]
+    fare_for_class = pclass_fares.mean()
+    fares_by_class[this_class] = fare_for_class
 
