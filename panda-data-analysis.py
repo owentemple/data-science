d2de4fd1c699ed55f6621d8cf2b09b49ff5fec97
owentemp <owentemple@gmail.com>
2017-08-01 21:18:16 -0500

    interpret results of multiple logistic regression

--- panda-data-analysis.py
+++ panda-data-analysis.py
@@ -281,6 +281,7 @@ for this_class in passenger_classes:
 # Confusion Matrix Section 11, Lecture 67
 # Interpreting coefficients of a logistic regression Section 11, Lecture 68
 # Let's build the model - first iteration Section 12, Lecture 72
+# Let's build the model - backward elimination: STEP-BY-STEP Section 12, Lecture 73
 
 
 
