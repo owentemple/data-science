a912a20fdbe5af1e8dba4aae77b6dcaff50e436e
owentemp <owentemple@gmail.com>
2017-09-16 10:02:56 -0500

    ex 7 & 8

---
+++ learnPythonTheHardWay/ex7.py
@@ -0,0 +1,10 @@
+print("Mary had a little lamb.")
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Mary went.")
+print("." * 10) #what'd that do?
+
+end1 = "C"
+end2 = "h"
+end12 = "z"
+
+print(end1 + end2 + end12, end= ' ') # changes the last character from newline to just a space
