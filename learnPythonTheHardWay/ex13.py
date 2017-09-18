bd2b38f4ac09a2a90f1aa5180cdb160426eab797
owentemp <owentemple@gmail.com>
2017-09-18 15:05:57 -0500

    use argv to assign parameters

---
+++ learnPythonTheHardWay/ex13.py
@@ -0,0 +1,14 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+
+x = input("WHat's your name?")
+y = input("What's your gender?")
+
+print(x)
+print(y)
