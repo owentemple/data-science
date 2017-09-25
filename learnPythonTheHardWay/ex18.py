e73649272b4811761d2b207eea2d560c4f0c9f1a
owentemp <owentemple@gmail.com>
2017-09-24 21:20:00 -0500

    ex18 LPTHW

---
+++ learnPythonTheHardWay/ex18.py
@@ -0,0 +1,19 @@
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+def print_none():
+    print("I got nothin'.")
+
+print_two("Zed", "Shaw")
+
+print_two_again("Zed","Shaw")
+
+print_one("First!")
+print_none()
