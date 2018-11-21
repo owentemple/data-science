72f33e5f68504e9f55011b6c9ac2bd8f687c86ee
Owen Temple <owentemple@gmail.com>
2018-11-21 13:21:20 -0600

    import part of package

--- datacamp/functions.py
+++ datacamp/functions.py
@@ -57,4 +57,16 @@ areas.reverse()
 print(areas)
 
 
+# Definition of radius
+r = 192500
 
+# Import radians function of math package
+from math import radians
+
+# Travel distance of Moon over 12 degrees. Store in dist.
+
+phi = radians(12)
+dist = r * phi
+
+# Print out dist
+print(dist)
