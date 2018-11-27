21a456f002c89fff9045bd6a49079d5056706872
Owen Temple <owentemple@gmail.com>
2018-11-27 16:16:52 -0600

    numpy

---
+++ datacamp/numpy.py
@@ -0,0 +1,32 @@
+# Create list baseball
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Import the numpy package as np
+import numpy as np
+
+# Create a numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out type of np_baseball
+print(type(np_baseball))
+
+
+
+# height is available as a regular list
+
+# Import numpy
+import numpy as np
+
+# Create a numpy array from height: np_height_in
+np_height_in = np.array(height_in)
+
+# Print out np_height
+print(np_height_in)
+
+# Convert np_height to m: np_height_m
+np_height_m = np_height_in * 0.0254
+
+# Print np_height_m
+print(np_height_m)
+
+
