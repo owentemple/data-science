c4c96ecbc11e2421658a45382a374d9e41360557
Owen Temple <owentemple@gmail.com>
2018-12-26 23:06:42 -0600

    numpy idexing

--- datacamp/numpy.py
+++ datacamp/numpy.py
@@ -65,3 +65,20 @@ print(light)
 
 # Print out BMIs of all baseball players whose BMI is below 21
 print(bmi[light])
+
+
+# height and weight are available as a regular lists
+
+# Import numpy
+import numpy as np
+
+# Store weight and height lists as numpy arrays
+np_weight_lb = np.array(weight_lb)
+np_height_in = np.array(height_in)
+
+# Print out the weight at index 50
+print(np_weight_lb[50])
+
+# Print out sub-array of np_height: index 100 up to and including index 110
+print(np_height_in[100:111])
+
