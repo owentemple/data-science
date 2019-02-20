404bbe299c893d29edc454bbebbcdaceb1b294f3
Owen Temple <owentemple@gmail.com>
2019-02-19 20:49:45 -0600

    some changes

--- datacamp/numpy.py
+++ datacamp/numpy.py
@@ -117,3 +117,20 @@ np_weight_lb = np_baseball[:,1]
 
 # Print out height of 124th player
 print(np_baseball[123])
+
+ updated is available as 2D numpy array
+
+# Import numpy package
+import numpy as np
+
+# Create np_baseball (3 cols)
+np_baseball = np.array(baseball)
+
+# Print out addition of np_baseball and updated
+print(np_baseball + updated)
+
+# Create numpy array: conversion
+conversion = np.array([0.0254, 0.453592, 1])
+
+# Print out product of np_baseball and conversion
+print(np_baseball * conversion)
