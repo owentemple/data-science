a3f44c68ad67411f71343d1d7226cc8db4401563
Owen Temple <owentemple@gmail.com>
2019-03-05 20:54:16 -0600

    after interpreter change

--- datacamp/numpy.py
+++ datacamp/numpy.py
@@ -149,3 +149,25 @@ print(np.mean(np_height_in))
 # Print out the median of np_height_in
 print(np.median(np_height_in))
 
+
+# np_baseball is available
+
+# Import numpy
+import numpy as np
+
+# Print mean height (first column)
+avg = np.mean(np_baseball[:,0])
+print("Average: " + str(avg))
+
+# Print median height. Replace 'None'
+med = np.median(np_baseball[:,0])
+print("Median: " + str(med))
+
+# Print out the standard deviation on height. Replace 'None'
+stddev = np.std(np_baseball[:,0])
+print("Standard Deviation: " + str(stddev))
+
+# Print out correlation between first and second column. Replace 'None'
+corr = np.corrcoef(np_baseball[:,0], np_baseball[:,1])
+print("Correlation: " + str(corr))
+
