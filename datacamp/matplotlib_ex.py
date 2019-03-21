b85a2e7c6fbc8ccbe21cbdd21e530971b3c06e87
Owen Temple <owentemple@gmail.com>
2019-03-20 22:59:35 -0500

    Update matplotlib_ex.py
    build plot

--- datacamp/matplotlib_ex.py
+++ datacamp/matplotlib_ex.py
@@ -11,3 +11,15 @@ plt.plot(year, pop)
 
 # Display the plot with plt.show()
 plt.show()
+
+
+# Print the last item of gdp_cap and life_exp
+print(gdp_cap[-1])
+print(life_exp[-1])
+
+
+# Make a line plot, gdp_cap on the x-axis, life_exp on the y-axis
+plt.plot(gdp_cap, life_exp)
+
+# Display the plot
+plt.show()
