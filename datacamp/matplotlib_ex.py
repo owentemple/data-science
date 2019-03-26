7e373ad9922d0c74e2a855a7360c3194a7b64e01
Owen Temple <owentemple@gmail.com>
2019-03-25 21:31:35 -0500

    scatter plot

--- datacamp/matplotlib_ex.py
+++ datacamp/matplotlib_ex.py
@@ -23,3 +23,12 @@ plt.plot(gdp_cap, life_exp)
 
 # Display the plot
 plt.show()
+
+# Change the line plot below to a scatter plot
+plt.scatter(gdp_cap, life_exp)
+
+# Put the x-axis on a logarithmic scale
+plt.xscale('log')
+
+# Show plot
+plt.show()
