56e036a2e4132d832d2fa14c376428cdcf33071b
Owen Temple <owentemple@gmail.com>
2019-04-18 20:53:16 -0500

    scatter plot

--- datacamp/matplotlib_ex.py
+++ datacamp/matplotlib_ex.py
@@ -33,5 +33,11 @@ plt.xscale('log')
 # Show plot
 plt.show()
 
-# continue
-# continue
+# Import package
+import matplotlib.pyplot as plt
+
+# Build Scatter plot
+plt.scatter(pop, life_exp)
+
+# Show plot
+plt.show()
