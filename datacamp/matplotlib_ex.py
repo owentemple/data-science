096e8bf00ee8520ca68639e48fd0180330cb2c94
Owen Temple <owentemple@gmail.com>
2019-05-04 21:48:09 -0500

    control bin size

--- datacamp/matplotlib_ex.py
+++ datacamp/matplotlib_ex.py
@@ -48,3 +48,17 @@ plt.hist(life_exp, bins = 10)
 
 # Display histogram
 plt.show()
+
+# Build histogram with 5 bins
+plt.hist(life_exp,bins=5)
+
+# Show and clean up plot
+plt.show()
+plt.clf()
+
+# Build histogram with 20 bins
+plt.hist(life_exp,bins=20)
+
+# Show and clean up again
+plt.show()
+plt.clf()
