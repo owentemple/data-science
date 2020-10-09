606d821ac98751279c69f4b65b299095e0af3ec3
Owen Temple <owentemple@gmail.com>
2020-10-08 21:07:47 -0500

    use if statements

--- datacamp/logic-control.py
+++ datacamp/logic-control.py
@@ -50,3 +50,16 @@ print(np.logical_or(my_house > 18.5, my_house < 10))
 
 # Both my_house and your_house smaller than 11
 print(np.logical_and(my_house < 11, your_house < 11))
+
+#
+# Define variables
+room = "kit"
+area = 14.0
+
+# if statement for room
+if room == "kit" :
+    print("looking around in the kitchen.")
+
+# if statement for area
+if area > 15:
+    print("big place!")
