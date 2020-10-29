2d36ec4c973d5717eb3bdbfaf1a5a47455460d73
Owen Temple <owentemple@gmail.com>
2020-10-28 21:28:06 -0500

    add second elif

--- datacamp/logic-control.py
+++ datacamp/logic-control.py
@@ -57,13 +57,22 @@ room = "kit"
 area = 14.0
 
 # if-else construct for room
+# Define variables
+room = "bed"
+area = 14.0
+
+# if-elif-else construct for room
 if room == "kit" :
     print("looking around in the kitchen.")
+elif room == "bed":
+    print("looking around in the bedroom.")
 else :
     print("looking around elsewhere.")
 
-# if-else construct for area
+# if-elif-else construct for area
 if area > 15 :
     print("big place!")
+elif area > 10 :
+    print("medium size, nice!")
 else :
     print("pretty small.")
