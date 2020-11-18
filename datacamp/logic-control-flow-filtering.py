af11e8311ab8afe4207ab2267280be795065f504
Owen Temple <owentemple@gmail.com>
2020-11-17 20:57:18 -0600

    subset using boolean

--- datacamp/logic-control-flow-filtering.py
+++ datacamp/logic-control-flow-filtering.py
@@ -6,4 +6,16 @@ cars = pd.read_csv('cars.csv', index_col = 0)
 sel = cars[cars['drives_right']]
 
 # Print sel
-print(sel)
+print(sel)# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Import numpy, you'll need this
+import numpy as np
+
+# Create medium: observations with cars_per_cap between 100 and 500
+
+
+
+
+# Print medium
