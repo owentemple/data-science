85abb001d9d2f26405cfeebd301c885ab35510f4
Owen Temple <owentemple@gmail.com>
2021-01-04 20:01:42 -0600

    use enumerate

--- datacamp/loops.py
+++ datacamp/loops.py
@@ -3,4 +3,10 @@ areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 
 # Code the for loop
 for item in areas:
-    print(item)
+    print(item)# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Change for loop to use enumerate() and update print()
+for index, a in enumerate(areas) :
+    print("room " + str(index) + ": " + str(a))
+    
