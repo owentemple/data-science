f5d6715cd62da09090c3f0c48e1153e78e978eae
Owen Temple <owentemple@gmail.com>
2021-01-14 19:58:34 -0600

    renumber rooms

--- datacamp/loops.py
+++ datacamp/loops.py
@@ -2,11 +2,5 @@
 areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 
 # Code the for loop
-for item in areas:
-    print(item)# areas list
-areas = [11.25, 18.0, 20.0, 10.75, 9.50]
-
-# Change for loop to use enumerate() and update print()
-for index, a in enumerate(areas) :
-    print("room " + str(index) + ": " + str(a))
-    
+for index, area in enumerate(areas) :
+    print("room " + str(index + 1) + ": " + str(area))
