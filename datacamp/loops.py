0194c63f4128c3dc86e0e1162eb0af6bdd021f2f
Owen Temple <owentemple@gmail.com>
2021-01-22 22:28:35 -0600

    write for loop

--- datacamp/loops.py
+++ datacamp/loops.py
@@ -1,6 +1,10 @@
-# areas list
-areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+# house list of lists
+house = [["hallway", 11.25],
+         ["kitchen", 18.0],
+         ["living room", 20.0],
+         ["bedroom", 10.75],
+         ["bathroom", 9.50]]
 
-# Code the for loop
-for index, area in enumerate(areas) :
-    print("room " + str(index + 1) + ": " + str(area))
+# Build a for loop from scratch
+for room in house:
+    print("the " + room[0] + " is " + str(room[1]) + " sqm")
