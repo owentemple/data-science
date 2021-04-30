81c484e0914c0e14d21a8942532c63c5629e94f5
Owen Temple <owentemple@gmail.com>
2021-04-29 19:25:11 -0500

    create steps with randit

--- datacamp/apply.py
+++ datacamp/apply.py
@@ -1,9 +1,20 @@
-# Import numpy and set seed
-import numpy as np
-np.random.seed(123)
+# Numpy is imported, seed is set
 
-# Use randint() to simulate a dice
-print(np.random.randint(1,7))
+# Starting step
+step = 50
 
-# Use randint() again
-print(np.random.randint(1,7))
+# Roll the dice
+dice = np.random.randint(1,7)
+
+
+# Finish the control construct
+if dice <= 2 :
+    step = step - 1
+elif dice <= 5 :
+    step = step + 1
+else :
+    step = step + np.random.randint(1,7)
+
+# Print out dice and step
+print(dice)
+print(step)
