03ec1f59059283ac49fb9cc99e3af174f9f87617
Owen Temple <owentemple@gmail.com>
2021-06-28 20:39:29 -0500

    implement clumsiness

--- datacamp/case-study.py
+++ datacamp/case-study.py
@@ -1,8 +1,8 @@
-# numpy and matplotlib imported, seed set.
+# numpy and matplotlib imported, seed set
 
-# initialize and populate all_walks
+# Simulate random walk 250 times
 all_walks = []
-for i in range(10) :
+for i in range(250) :
     random_walk = [0]
     for x in range(100) :
         step = random_walk[-1]
@@ -13,25 +13,15 @@ for i in range(10) :
             step = step + 1
         else:
             step = step + np.random.randint(1,7)
+
+        # Implement clumsiness
+        if np.random.rand() <= 0.001:
+            step = 0
+
         random_walk.append(step)
     all_walks.append(random_walk)
 
-# Convert all_walks to Numpy array: np_aw
-np_aw = np.array(all_walks)
-
-
-
-# Plot np_aw and show
-plt.plot(np_aw)
-plt.show()
-
-# Clear the figure
-plt.clf()
-
-# Transpose np_aw: np_aw_t
-np_aw_t = np.transpose(np_aw)
-
-
-# Plot np_aw_t and show
+# Create and plot np_aw_t
+np_aw_t = np.transpose(np.array(all_walks))
 plt.plot(np_aw_t)
 plt.show()
