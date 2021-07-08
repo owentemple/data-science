3fdc60b101c50eb4df1fd92ec5a3900a6424cf22
Owen Temple <owentemple@gmail.com>
2021-07-07 21:41:12 -0500

    run

--- datacamp/case-study.py
+++ datacamp/case-study.py
@@ -1,8 +1,8 @@
 # numpy and matplotlib imported, seed set
 
-# Simulate random walk 250 times
+# Simulate random walk 500 times
 all_walks = []
-for i in range(250) :
+for i in range(500) :
     random_walk = [0]
     for x in range(100) :
         step = random_walk[-1]
@@ -13,15 +13,17 @@ for i in range(250) :
             step = step + 1
         else:
             step = step + np.random.randint(1,7)
-
-        # Implement clumsiness
-        if np.random.rand() <= 0.001:
+        if np.random.rand() <= 0.001 :
             step = 0
-
         random_walk.append(step)
     all_walks.append(random_walk)
 
 # Create and plot np_aw_t
 np_aw_t = np.transpose(np.array(all_walks))
-plt.plot(np_aw_t)
+
+# Select last row from np_aw_t: ends
+ends = np_aw_t[-1,:]
+
+# Plot histogram of ends, display plot
+plt.hist(ends)
 plt.show()
